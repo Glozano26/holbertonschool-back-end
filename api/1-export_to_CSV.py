20a2a11610a9f63a22f42d69763513b90cbe7c99
glozano26 <lozsuperbia2014@gmail.com>
2023-12-20 19:46:49 -0500

    Export to CSV v2

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """"Using what you did in the task #0, extend your Python script to export
 data in the CSV format"""
+import csv
 import requests
 import sys
-import csv
 
 
 def employee_todo(employee_id):
